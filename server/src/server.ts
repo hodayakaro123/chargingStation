bce189480928da19ce917b1ca8efec6a29d62401
Tom Guter <128406348+TomGuter@users.noreply.github.com>
2025-01-17 22:00:53 +0200

    book tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import addChargingStation from "./routes/charger_route";
 import cors from "cors";
 import addComments from "./routes/commentsOnCharger_route";
 import userRouter from "./routes/user_route";
+import bookCharger from "./routes/book_a_charger_route";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 import geminiRouter from "./routes/gemini_route";
@@ -56,6 +57,7 @@ const moduleApp = async (): Promise<Express> => {
   app.use("/addChargingStation", addChargingStation);
   app.use("/addComments", addComments);
   app.use("/gemini", geminiRouter);
+  app.use("/bookings", bookCharger);
 
 
   return app;
